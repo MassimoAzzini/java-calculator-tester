5c73e89a4b93c910c5e37423954435d2dc3dc07d
MassimoAzzini <massimo.azzini.web@gmail.com>
2024-04-10 17:17:32 +0200

    [add] fix double in BigDecimal

--- src/main/java/Ticket/Ticket.java
+++ src/main/java/Ticket/Ticket.java
@@ -1,14 +1,16 @@
 package Ticket;
 
+import java.math.BigDecimal;
+
 public class Ticket {
-    private final double priceKm = 0.21;
-    private final double discountUnderage = 0.2;
-    private final double discountOver65 = 0.4;
+    private final BigDecimal priceKm = new BigDecimal("0.21");
+    private final BigDecimal discountUnderage = new BigDecimal("0.2").setScale(2,4);
+    private final BigDecimal discountOver65 = new BigDecimal("0.4").setScale(2,4);
 
     private int kmTravel;
     private int ageUsers;
 
-    private double finalPrice;
+    private BigDecimal finalPrice;
 
     public Ticket(int kmTravel, int ageUsers) throws IllegalArgumentException {
         if(kmTravel <= 0 || ageUsers <= 0) throw new IllegalArgumentException();
@@ -16,24 +18,24 @@ public class Ticket {
         this.ageUsers = ageUsers;
     }
 
-    public double finalPrice (){
+    public BigDecimal finalPrice (){
 
-        double totalPrice = kmTravel * priceKm;
+        BigDecimal totalPrice = new BigDecimal(kmTravel).multiply(priceKm);
 
         if (ageUsers < 18){
-            finalPrice = totalPrice - (totalPrice * discountUnderage);
-            System.out.println("Il prezzo totale del biglietto è: " + finalPrice + "€");
-            System.out.println("Hai ottenuto uno sconto MINORENNI pari al " + (int)(discountUnderage * 100) +"%");
+            finalPrice = totalPrice.subtract(totalPrice.multiply(discountUnderage));
+            System.out.println("Il prezzo totale del biglietto è: " + finalPrice.setScale(2,4) + "€");
+            System.out.println("Hai ottenuto uno sconto MINORENNI pari al " + discountUnderage.multiply(new BigDecimal(100)).setScale(0,2) +"%");
         } else if (ageUsers >= 65){
-            finalPrice = totalPrice - (totalPrice * discountOver65);
-            System.out.println("Il prezzo totale del biglietto è: " + finalPrice + "€");
-            System.out.println("Hai ottenuto uno sconto OVER65 pari al " + (int)(discountOver65 * 100) +"%");
+            finalPrice = totalPrice.subtract(totalPrice.multiply(discountOver65));
+            System.out.println("Il prezzo totale del biglietto è: " + finalPrice.setScale(2,4) + "€");
+            System.out.println("Hai ottenuto uno sconto OVER65 pari al " + discountOver65.multiply(new BigDecimal(100)).setScale(0,2) +"%");
         } else {
             finalPrice = totalPrice;
-            System.out.println("Il prezzo totale del biglietto è: " + finalPrice + "€");
+            System.out.println("Il prezzo totale del biglietto è: " + finalPrice.setScale(2,4) + "€");
         }
 
-        return finalPrice;
+        return finalPrice.setScale(2,4);
     }
 
 
--- src/test/java/Ticket/TicketTest.java
+++ src/test/java/Ticket/TicketTest.java
@@ -2,6 +2,8 @@ package Ticket;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketTest {
@@ -22,8 +24,8 @@ class TicketTest {
         Ticket ticket1 = new Ticket(100, 25);
         Ticket ticket2 = new Ticket(100, 15);
         Ticket ticket3 = new Ticket(100, 68);
-        assertEquals(21, ticket1.finalPrice());
-        assertEquals(16.8, ticket2.finalPrice());
-        assertEquals(12.6, ticket3.finalPrice());
+        assertEquals(new BigDecimal("21.00"), ticket1.finalPrice());
+        assertEquals(new BigDecimal("16.80"), ticket2.finalPrice());
+        assertEquals(new BigDecimal("12.60"), ticket3.finalPrice());
     }
 }
